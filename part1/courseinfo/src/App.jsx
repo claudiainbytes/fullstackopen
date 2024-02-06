deaf2ba7bbc78e888a8210299467514c4968035b
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-06 17:16:37 -0500

    Part 1. Refactor App creating Header, Content, Total components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,27 @@
+const Header = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+const Content = () => {
+    
+  return (
+    <>
+    </>
+  )
+}
+
+const Total = () => {
+    
+  return (
+    <>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
