c87d24d4a8f846df7458d0a9a7861436f46cdce0
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-06 17:21:11 -0500

    Part 1. Header, Content, Total components created with JSX structure

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,39 +1,21 @@
 const Header = () => {
-
+  const course = 'Half Stack application development'
   return (
     <>
+      <h1>{course}</h1>
     </>
   )
 }
 
 const Content = () => {
-    
-  return (
-    <>
-    </>
-  )
-}
-
-const Total = () => {
-    
-  return (
-    <>
-    </>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
   return (
-    <div>
-      <h1>{course}</h1>
+    <>
       <p>
         {part1} {exercises1}
       </p>
@@ -43,7 +25,31 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
+    </>
+  )
+}
+
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return (
+    <>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  
+  
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
     </div>
   )
 }
