e262bd73e8054d31346a9dfe8257fd0a4f719639
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-06 17:36:53 -0500

    Part 1. Header, Content, Total components were added props parameter

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,55 +1,38 @@
-const Header = () => {
-  const course = 'Half Stack application development'
+const Header = (props) => {
   return (
     <>
-      <h1>{course}</h1>
+      <h1>{props.course}</h1>
     </>
   )
 }
 
-const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Content = (props) => {
   return (
     <>
       <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
+        {props.part} {props.exercise}
       </p>
     </>
   )
 }
 
-const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
     </>
   )
 }
 
 const App = () => {
-  
-  
-  const exercises3 = 14
-
+  const exercises = [10, 7, 14];
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course="Half Stack application development"/>
+      <Content part="Fundamentals of React" exercise="10"/>
+      <Content part="Using props to pass data" exercise="7"/>
+      <Content part="State of a component" exercise="14"/>
+      <Total exercises={exercises}/>
     </div>
   )
 }
