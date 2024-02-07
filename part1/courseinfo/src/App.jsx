e9a5cc388833fa6b9f379086a3fc4c73f384ea97
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-07 15:57:59 -0500

    Part 1.4. Modify component App. Modify parts variable in an array of objects, Content and Total comonents were modified

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -29,38 +29,40 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props.exercises)
+  console.log(props.parts)
+  let total = 0
+  props.parts.forEach((part) => { total += part.exercises })
+  console.log("Total: " + total)
   return (
     <>
-      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+      <p>Number of exercises {total}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
 
-  const parts = [part1, part2, part3]
-
-  const exercises = [part1.exercises, part2.exercises, part3.exercises]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
       <Content parts={parts}/>
-      <Total exercises={exercises}/>
+      <Total parts={parts}/>
     </div>
   )
 }
