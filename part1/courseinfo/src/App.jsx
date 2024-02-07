af518b17079a297cf628c77e5fb3f8de58bcfa62
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-07 14:55:21 -0500

    Part 1.3. Modify component App. Initial state given in exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -35,15 +35,23 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const exercises = [10, 7, 14];
-  const parts = [{ part: "Fundamentals of React", exercise: 10 }, 
-                    { part: "Using props to pass data", exercise: 7 }, 
-                    { part: "State of a component", exercise: 14 }];
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-      <Header course="Half Stack application development"/>
-      <Content parts={parts}/>
-      <Total exercises={exercises}/>
+      ...
     </div>
   )
 }
