ff8484f91f680e4c2d655a9f843a92e735121813
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-12 16:22:06 -0500

    Part 1.6 Unicafe. Step 2. Handler functions to set statistics states

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,32 +3,86 @@ import { useState } from 'react'
 // Button component defined in a simplified way
 const Button = ({name, handleClick}) => <button onClick={handleClick}>{name}</button>
 
+// Statistics component
+const Statistics = ({all, average, positive}) => {
+  return(
+      <>
+        all {all} <br/>
+        average {average} <br/>
+        positive {positive} %
+      </>
+  )
+}
+
 const App = () => {
   
-  // save clicks of each button to its own state
+  // save clicks of each button to its own state, each feedback has two properties, number of collected and score
+
+  const [good, setGood] = useState({ collected: 0, score: 1})
+  const [neutral, setNeutral] = useState({ collected: 0, score: 0})
+  const [bad, setBad] = useState({ collected: 0, score: -1})
+
+  // statistics states
+
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  // setters to update each statistic state
   
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // Sum all collected feedbacks
+  const setToAll = (arrCollected) => {
+    const newAll =  arrCollected.reduce((accumulator, currentVal) => accumulator + currentVal, 0) 
+    setAll(newAll)
+    return newAll
+  }
+
+  // Average of feedbacks according to their score
+  const setToAverage = (arrState, newAll) => {
+    const scores = arrState.map((obj) => obj.collected * obj.score )
+    const newAverage = scores.reduce((accumulator, currentVal) => accumulator + currentVal, 0) / newAll
+    setAverage(newAverage)
+  }
+
+  // Percentaje of positive feedbacks related to all feedbacks
+  const setToPositive = (newGood, newAll) => {
+    const newPositive = (newGood.collected * 100) / newAll
+    setPositive(newPositive)
+  }
   
-  // handlers to set the state of each comment
+  // handlers to set the state of each comment usign Spread Operator to update the state
 
   const handleToGood = () => {
-    console.log("feedback to good")
-    const newGood = good + 1
+    const newGood = {
+      ...good,
+      collected: good.collected + 1
+    }
     setGood(newGood)
+    const newAll = setToAll([ newGood.collected, neutral.collected, bad.collected ])
+    setToAverage([newGood, neutral, bad], newAll)
+    setToPositive(newGood, newAll)
   }
 
   const handleToNeutral = () => {
-    console.log("feedback to neutral")
-    const newNeutral = neutral + 1
+    const newNeutral = {
+      ...neutral,
+      collected: neutral.collected + 1
+    }
     setNeutral(newNeutral)
+    const newAll = setToAll([ good.collected, newNeutral.collected, bad.collected ])
+    setToAverage([good, newNeutral, bad], newAll)
+    setToPositive(good, newAll)
   }
 
   const handleToBad = () => {
-    console.log("feedback to bad")
-    const newBad = bad + 1
+    const newBad = {
+      ...bad,
+      collected: bad.collected + 1
+    }
     setBad(newBad)
+    const newAll = setToAll([ good.collected, neutral.collected, newBad.collected ])
+    setToAverage([good, neutral, newBad], newAll)
+    setToPositive(good, newAll)
   }
 
   return (
@@ -38,9 +92,10 @@ const App = () => {
       <Button name="neutral" handleClick={handleToNeutral}/>&nbsp;
       <Button name="bad" handleClick={handleToBad}/>&nbsp;
       <h1>statistics</h1>
-      <p>good {good} <br/>
-         neutral {neutral} <br/>
-         bad {bad}
+      <p>good {good.collected} <br/>
+         neutral {neutral.collected} <br/>
+         bad {bad.collected} <br/>
+         <Statistics all={all} average={average} positive={positive}/>
       </p>
     </div>
   )
