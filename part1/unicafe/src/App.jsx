014486aff72a031d79fd6b66bbfe6cdfd5c36618
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-12 12:05:17 -0500

    Part 1.6 Unicafe. Step 1. Create Button component and handlers to set each feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,46 @@
 import { useState } from 'react'
 
+// Button component defined in a simplified way
+const Button = ({name, handleClick}) => <button onClick={handleClick}>{name}</button>
+
 const App = () => {
   
   // save clicks of each button to its own state
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
+  // handlers to set the state of each comment
+
+  const handleToGood = () => {
+    console.log("feedback to good")
+    const newGood = good + 1
+    setGood(newGood)
+  }
+
+  const handleToNeutral = () => {
+    console.log("feedback to neutral")
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+  }
+
+  const handleToBad = () => {
+    console.log("feedback to bad")
+    const newBad = bad + 1
+    setBad(newBad)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>&nbsp;
-      <button>neutral</button>&nbsp;
-      <button>bad</button>&nbsp;
+      <Button name="good" handleClick={handleToGood}/>&nbsp;
+      <Button name="neutral" handleClick={handleToNeutral}/>&nbsp;
+      <Button name="bad" handleClick={handleToBad}/>&nbsp;
       <h1>statistics</h1>
-      <p>good 6 <br/>
-         neutral 2 <br/>
-         bad 1
+      <p>good {good} <br/>
+         neutral {neutral} <br/>
+         bad {bad}
       </p>
     </div>
   )
