3d1ae2355441e70144a61b82d158e198514e1434
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-12 11:12:52 -0500

    Part 1.6 Unicafe. Step 0. Adding base structure

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+
 const App = () => {
+  
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
   return (
     <div>
-      
+      <h1>give feedback</h1>
+      <button>good</button>&nbsp;
+      <button>neutral</button>&nbsp;
+      <button>bad</button>&nbsp;
+      <h1>statistics</h1>
+      <p>good 6 <br/>
+         neutral 2 <br/>
+         bad 1
+      </p>
     </div>
   )
 }
