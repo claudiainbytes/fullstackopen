607b28c08fdfa5ac7b863b5c62d5bda3d5acf20a
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-16 17:21:35 -0500

    Commit: Part 2.9 Step 4. Phonebook. Search field to filter the list of people by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,10 +4,14 @@ import Person from './components/Person'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterPerson, setFilterPerson] = useState('')
 
   const isPersonExist = (name) => persons.find((person) => person.name === name.trim()) ? true : false
 
@@ -16,6 +20,12 @@ const App = () => {
   const handleNameChange = (event) => setNewName(event.target.value)
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const filteredPersons = (filterPerson.trim().length === 0 || filterPerson.trim() === '') ? persons : persons.filter((person) => person.name.includes(filterPerson) )
+
+  const handleFilterPersonByNameChange = (event) => {
+    setFilterPerson(event.target.value)
+  }
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,6 +46,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterPerson} onChange={handleFilterPersonByNameChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:  <input value={newName} onChange={handleNameChange}/>
@@ -49,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      { persons.map((person) => <Person key={person.name} person={person}/> ) }
+      { filteredPersons.map((person) => <Person key={person.name} person={person}/> ) }
       </ul>
     </div>
   )
