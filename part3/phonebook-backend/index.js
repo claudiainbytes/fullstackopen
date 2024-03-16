c380da77e86cd982752e90deba7f6188e733370a
claudiainbytes <claudiaeforero@hotmail.com>
2024-03-16 00:25:57 -0500

    Part 3.17 Step 5. Phonebook. Mongo DB.*

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -98,6 +98,21 @@ app.post('/api/persons', (request, response) => {
         })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.get('/api/info', async(request, response) => {
     const date = new Date()
     const total = await Person.countDocuments() 
