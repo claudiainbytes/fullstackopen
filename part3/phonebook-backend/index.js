f81a3d72deaf36acd5e9daf368c0afdcb83abf2b
claudiainbytes <claudiaeforero@hotmail.com>
2024-03-15 23:44:14 -0500

    Part 3.16 Step 4. Phonebook. Mongo DB

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,9 +4,31 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person') 
 
+morgan.token('body', function (req, res) { return JSON.stringify({ "body": req.body }) } )
+
+morgan.token('respon', function (req, res) { return JSON.stringify({ "persons": persons })} )
+
+const customMorgan = ':method :url :status :response-time ms - :res[content-length] :body - :respon'
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const app = express()
 app.use(cors())
+app.use(express.json())
 app.use(express.static('dist'))
+app.use(morgan(customMorgan))
 
 let persons = []
 
@@ -18,48 +40,27 @@ app.get('/api/persons', (request, response) => {
         })
 })
 
-app.use(express.json())
-
-morgan.token('body', function (req, res) { return JSON.stringify({ "body": req.body }) } )
-
-morgan.token('respon', function (req, res) { return JSON.stringify({ "persons": persons })} )
-
-const customMorgan = ':method :url :status :response-time ms - :res[content-length] :body - :respon'
-
-app.use(morgan(customMorgan))
-
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person
-        .findById(request.params.id)
-        .then(person => {
-           if(person) {
-            response.json(person)
-           } else {
-            response.status(404).end()
-           }
-        })
-        .catch(err => { 
-          console.log(err)
-          response.status(400).send({ error: 'malformatted id'})
-        })
+          .findById(request.params.id)
+          .then(person => {
+            if(person) {
+              response.json(person)
+            } else {
+              response.status(404).end()
+            }
+          })
+          .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person
         .findByIdAndDelete(request.params.id)
         .then(result => {
           response.status(204).end()
         })
-        .catch(err => { 
-          console.log(err)
-        })
+        .catch(error => next(error))
 })
-
-const generateRandomId = () => {
-  const randomId = Math.floor((Math.random() * 100) + 1)
-  const lastId = Math.max(...persons.map(person => person.id))
-  return persons.filter(person => person.id === randomId).length === 0 ? randomId : (lastId + 1) 
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -103,6 +104,9 @@ app.get('/api/info', async(request, response) => {
     const message = `<p>Phonebook has info for ${total} people<br/>${ date } </p>`
       response.send(message)
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
