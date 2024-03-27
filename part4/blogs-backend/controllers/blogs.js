10f14bf833c477f0025714c384a251f6b10e28a0
claudiainbytes <claudiaeforero@hotmail.com>
2024-03-27 17:37:07 -0500

    Part 4.15 to 4.23. Steps 5

--- part4/blogs-backend/controllers/blogs.js
+++ part4/blogs-backend/controllers/blogs.js
@@ -1,15 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const { error } = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)     
 })
 
 blogsRouter.post('/', async (request, response) => {
    
-    const { title, author, url, likes } = request.body
+    const { title, author, url, likes, userId } = request.body
+
+    const user = await User.findById(userId)
 
     if ((title === undefined) || (title.length === 0)) {
         return response.status(400).json({
@@ -35,8 +38,10 @@ blogsRouter.post('/', async (request, response) => {
             error: 'title already exists'
         })
     } else {
-        const blog = new Blog( { title, author, url, likes })
+        const blog = new Blog( { title, author, url, likes, user: user.id })
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(201).json(savedBlog)
     }
 
