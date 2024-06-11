b1a3d155afb9ecc16953cf81e2113c3054a5ce26
claudiainbytes <claudiaeforero@hotmail.com>
2024-06-10 20:03:44 -0500

    Part 7. Exercises 7_1 and 7_3

---
+++ part7/README.md
@@ -0,0 +1,9 @@
+# [Full Stack open 2024](/)
+
+[https://fullstackopen.com](https://fullstackopen.com)
+
+## [Part 7. React router, custom hooks, styling app with CSS and webpack](https://fullstackopen.com/en/part7)
+
+Exercises 
+
+- [routed-anecdotes](/part7/routed-anecdotes)
---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+import {
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch
+} from "react-router-dom"
+
+
+import Menu from './components/Menu'
+import NotificationMessage from './components/Notification'
+import AnecdoteList from './components/AnecdoteList'
+import Anecdote from './components/Anecdote'
+import About from './components/About'
+import CreateNew from './components/CreateNew'
+import Footer from './components/Footer'
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState({ message: '', classname: 'success' })
+
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match 
+    ? anecdotes.find(a => a.id === Number(match.params.id))
+    : null
+  
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification({ message: `a new anecdote ${anecdote.content} was created`, classname: 'success' })
+    setTimeout(() => {
+      setNotification({ message: '', classname: 'sucess' })
+    }, 5000)
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      { notification.message.length > 0
+          ? <NotificationMessage message={notification} />
+          : <></>
+      }
+      <Routes>
+        <Route path="/anecdotes/:id" element={ <Anecdote anecdote={anecdote} />} />
+        <Route path="/anecdotes" element={ <AnecdoteList anecdotes={anecdotes} /> } />
+        <Route path="/about" element={ <About />} />
+        <Route path="/create" element={ <CreateNew addNew={addNew} />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
+
+/*
+  setMessage({ message: error.response.data.error, classname:'error' })
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+
+*/
