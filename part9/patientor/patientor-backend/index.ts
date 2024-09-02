dcd0362e9336b373a6b9b8a775ca765427d1f157
claudiainbytes <claudiaeforero@hotmail.com>
2024-09-02 10:22:36 -0500

    Part 9. Exercise 9_9 Adjusting CORS

--- part9/patientor/patientor-backend/index.ts
+++ part9/patientor/patientor-backend/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import { Request, Response } from 'express';
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-
-app.get('/ping', (_req, res) => {
+app.get('/ping', (_req: Request, res: Response) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
